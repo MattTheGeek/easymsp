522ecd862c8a6a39ac18399daf74478e9cc40683
unknown <Matthew@Matthew-PC.(none)>
2012-02-16 10:48:48 -0800

    Added barebone system.c

---
+++ current/library/msp430x5xx/system.c
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+	Interrupt Service Routines
+	
+	Handles non-maskable interrupts.
+*/
+
+void interrupt system_nmi_isr(void)
+{
+
+}
+
+void interrupt user_nmi_isr(void)
+{
+
+}
