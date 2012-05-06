834fe69e7bb0e7ecb5db6b2fd0fea007f2d7c899
Matthew Burmeister <matthewburmeister@gmail.com>
2012-05-05 22:50:36 -0700

    Updated device file

--- library/device.h
+++ library/device.h
@@ -1,38 +1,38 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
-#if (defined MSP430F5510) == 1 || (defined __MSP430F5510__) == 1 || (defined  __MSP430F5510) == 1
+#if (defined MSP430F5510) || (defined __MSP430F5510__) || (defined  __MSP430F5510)
 #	include "device/msp430f5510.h"
 
-#	if VERBOSE == YES
+#	if (VERBOSE == YES)
 		asm("	.mmsg	\"Device is MSP430F5510\"");
 #	endif
 		
-#elif (defined MSP430G2553) == 1 || (defined __MSP430G2553__) == 1 || (defined __MSP430G2553) == 1
+#elif (defined MSP430G2553) || (defined __MSP430G2553__) || (defined __MSP430G2553) 
 #	include "device/msp430g2553.h"
 		
-#	if VERBOSE == YES
+#	if (VERBOSE == YES)
 		asm("	.mmsg	\"Device is MSP430G2553\"");
 #	endif
 		
 #elif (defined MSP430G2452) || (defined __MSP430G2452__) || (defined __MSP430G2452)
 #	include "device/msp430g2452.h"
 		
-#	if VERBOSE == YES
+#	if (VERBOSE == YES)
 		asm("	.mmsg	\"Device is MSP430G2452\"");
 #	endif
 		
 #elif (defined MSP430G2231) || (defined __MSP430G2231__) || (defined __MSP430G2231)
 #	include "device/msp430g2231.h"
 		
-#	if VERBOSE == YES
+#	if (VERBOSE == YES)
 		asm("	.mmsg	\"Device is MSP430G2231\"");
 #	endif
 
 #elif (defined MSP430G2211) || (defined __MSP430G2211__) || (defined __MSP430G2211)
 #	include "device/msp430g2211.h"
 		
-#	if VERBOSE == YES
+#	if (VERBOSE == YES)
 		asm("	.mmsg	\"Device is MSP430G2211\"");
 #	endif
 		
@@ -40,23 +40,23 @@
 #	error "EasyMSP is not supported with this device. Please double check your define."
 #endif
 
-#if VERBOSE == YES
+#if (VERBOSE == YES)
 		
-#	if SERIES == 1
+#	if (SERIES == 1)
 		asm("	.mmsg	\"Series is F1xx line. \"");
-#	elif SERIES == 2
+#	elif (SERIES == 2)
 		asm("	.mmsg	\"Series is F2xx line OR G2xx Value Line. \"");
-#	elif SERIES == 3
+#	elif (SERIES == 3)
 		asm("	.mmsg	\"Series is F3xx line. \"");
-#	elif SERIES == 4
+#	elif (SERIES == 4)
 		asm("	.mmsg	\"Series is F4xx line. \"");
-#	elif SERIES == 5
+#	elif (SERIES == 5)
 		asm("	.mmsg	\"Series is F5xx Performance line. \"");
-#	elif SERIES == 6
+#	elif (SERIES == 6)
 		asm("	.mmsg	\"Series is F6xx Performance line. \"");
-#	elif SERIES == 'FRAM'
+#	elif (SERIES == 'FRAM')
 		asm("	.mmsg	\"Series is FRAM Line \"");
-#	elif SERIES == 'VOLVERINE'
+#	elif (SERIES == 'VOLVERINE')
 		asm("	.mmsg	\"Series is VOLVERINE \"");
 #	endif
 
