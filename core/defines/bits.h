10bebdee81316f4d1e91007efd0039f9d029f2a9
Matthew Burmeister <matthewburmeister@gmail.com>
2014-01-28 13:14:13 -0800

    Starting over and rewriting EasyMSP for the new year. Version changed to 2.0. Codename EasyMSP2.

---
+++ core/defines/bits.h
@@ -0,0 +1,79 @@
+#ifndef BIT0
+#	define BIT0 (1 << 0)
+#endif
+
+#ifndef BIT1
+#	define BIT1 (1 << 1)
+#endif
+
+#ifndef BIT2
+#	define BIT2 (1 << 2)
+#endif
+
+#ifndef BIT3
+#	define BIT3 (1 << 3)
+#endif
+
+#ifndef BIT4
+#	define BIT4 (1 << 4)
+#endif
+
+#ifndef BIT5
+#	define BIT5 (1 << 5)
+#endif
+
+#ifndef BIT6
+#	define BIT6 (1 << 6)
+#endif
+
+#ifndef BIT7
+#	define BIT7 (1 << 7)
+#endif
+
+#ifndef BIT8
+#	define BIT8 (1 << 8)
+#endif
+
+#ifndef BIT9
+#	define BIT9 (1 << 9)
+#endif
+
+#ifndef BIT10
+#	define BIT10 (1 << 10)
+#endif
+
+#ifndef BIT11
+#	define BIT11 (1 << 11)
+#endif
+
+#ifndef BIT12
+#	define BIT12 (1 << 12)
+#endif
+
+#ifndef BIT13
+#	define BIT13 (1 << 13)
+#endif
+
+#ifndef BIT14
+#	define BIT14 (1 << 14)
+#endif
+
+#ifndef BIT15
+#	define BIT15 (1 << 15)
+#endif
+
+#ifndef BIT16
+#	define BIT16 (1 << 16)
+#endif
+
+#ifndef BIT17
+#	define BIT17 (1 << 17)
+#endif
+
+#ifndef BIT18
+#	define BIT18 (1 << 18)
+#endif
+
+#ifndef BIT19
+#	define BIT19 (1 << 19)
+#endif
---
+++ core/preinit.c
@@ -0,0 +1,81 @@
+/*
+ * preinit
+ * Preinit code for the EasyMSP core
+ *
+ * Version 1
+ *
+ * www.code.google.com/p/easymsp/
+ *
+ * Copyright 2014 Matthew Burmeister
+ *
+ * Created on 01/28/2014
+ *
+ */
+
+/* DESCRIPTION
+ *
+ * Does setup before calling the users code.
+ */
+
+
+/* DESCRIPTION
+ *
+ * [What is this?]
+ *
+ * [What does it do?]
+ *
+ * [how does it do it?]
+ *
+ * [Anything special?]
+ */
+
+/* CONTACT
+ *
+ * If you have any questions or comments; Please feel free to open an issue at
+ * www.code.google.com/p/easymsp/issues/entry
+ *
+ * If you wish to communiate privatly, feel free to send me an email to:
+ * matthewburmeister ({a}t) [gmail] (dot) com.
+ *
+ * Humans: Decode the email using your human skills. Remove spaces, {}, and
+ * []'s. Translate things in () to the symbol form.
+ * Robots: No.
+ *
+ * Lastly, you may send mail to:
+ *
+ * Matthew Burmeister
+ * ATTN: EasyMSP
+ * 11593 AUTUNNO ST
+ * LAS VEGAS NV  89183
+ * UNITED STATES OF AMERICA
+ *
+ * It's great getting physical mail time to time.
+ */
+
+/* LEGAL
+ *
+ * EasyMSP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * EasyMSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
+/* CHANGE LOG
+ *
+ */
+
+inline void easymsp_preInit()
+{
+	return;
+}
